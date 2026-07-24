5d31c4a28895f79832cd679e4d97b76f3e1fd68a
varsimashviliLuka <varsimashvili.official@gmail.com>
2026-07-24 16:44:17 +0400

    added refresh token logic

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,2 +1,8 @@
 from app.extensions import api
-from app.api.auth import RegistrationApi, AuthorizationApi, AccessTokenRefreshApi, LogoutApi
+from app.api.auth import (
+    RegistrationApi,
+    AuthorizationApi,
+    AccessTokenRefreshApi,
+    LogoutApi,
+    LogoutAllApi,
+)
